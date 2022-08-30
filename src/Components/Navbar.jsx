262043121809ff6641959e4b1cd98825f4d2f415
J-Prajwal <luckyjais058@gmail.com>
2022-08-30 15:33:03 +0530

    cart icon with count bubble added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,6 +17,8 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
+  Icon,
+  chakra,
 } from "@chakra-ui/react";
 import {
   HamburgerIcon,
@@ -101,7 +103,37 @@ export default function Navbar() {
               {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
             </Button>
             <Button colorScheme={"none"} size={["lg", "2xl"]} px={5}>
-              <ImCart color={colorMode === "light" ? "black" : "white"} />
+              <chakra.span pos="relative" display="inline-block">
+                <Icon
+                  color={colorMode == "light" ? "black" : "white"}
+                  boxSize={6}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                </Icon>
+                <chakra.span
+                  pos="absolute"
+                  top="-1px"
+                  right="-1px"
+                  px={2}
+                  py={1}
+                  fontSize="xs"
+                  fontWeight="bold"
+                  lineHeight="none"
+                  color="red.100"
+                  transform="translate(50%,-50%)"
+                  bg="red.600"
+                  rounded="full"
+                >
+                  99
+                </chakra.span>
+              </chakra.span>
+              ;
             </Button>
 
             {isAuth ? (
